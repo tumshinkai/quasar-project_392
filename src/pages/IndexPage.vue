a0fb0927ffd3dfa5918d8007a4eee4cacff4c4ea
TumTummy <141399784+tumshinkai@users.noreply.github.com>
2023-10-07 22:35:33 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+  <InputPage/>
+  <FunctionPage/>
+</template>
+
+
+<script>
+import InputPage from './InputPage.vue';
+import FunctionPage from './FunctionPage.vue'
+
+export default {
+  name: 'HtmlComponent',
+  components:{
+    InputPage
+    ,FunctionPage
+  },
+  data() {
+    return {
+      title: 'Sirawit Hannarong',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ test/jest/__tests__/FunctionPage.spec.js
@@ -0,0 +1,67 @@
+import FunctionPage from 'src/pages/FunctionPage.vue'
+import { shallowMount } from '@vue/test-utils'
+
+describe('IndexPage', () => {
+  it('should render correct contents', () => {
+    const wrapper = shallowMount(FunctionPage)
+    let header = wrapper.find('.htmlClass h1')
+    expect(header.exists()).toBe(true)
+    expect(header.text())
+      .toBe('Sirawit Hannarong')
+  })
+})
+
+it('check text content to be as defined in variable', () => {
+  const wrapper = shallowMount(FunctionPage, {
+    data () {
+      return {
+        title: 'I love Vue.'
+      }
+    }
+  })
+  let header = wrapper.find('.htmlClass h1')
+  expect(header.text()).toBe('I love Vue.')
+})
+
+test('should show the form element on the user output', () => {
+  const wrapper = shallowMount(FunctionPage)
+  expect(wrapper.find('form').exists()).toBe(true)
+})
+
+test('should contain input fields in template', () => {
+  const wrapper = shallowMount(FunctionPage)
+  expect(wrapper.find('form > input').exists()).toBe(true)
+})
+
+test('should have button', () => {
+  const wrapper = shallowMount(FunctionPage)
+  expect(wrapper.find('button').exists()).toBe(true)
+})
+
+test('trigger click event on button ', async () => {
+  const wrapper = shallowMount(FunctionPage)
+  const button = wrapper.find('button')
+  await button.trigger('click')
+  expect(wrapper.vm.addition(1,2)).toBe(3)
+})
+
+test('trigger click event on button ', async () => {
+  const wrapper = shallowMount(FunctionPage)
+  const button = wrapper.find('button')
+  await button.trigger('click')
+  expect(wrapper.vm.subtraction(2,1)).toBe(1)
+})
+
+test('trigger click event on button ', async () => {
+  const wrapper = shallowMount(FunctionPage)
+  const button = wrapper.find('button')
+  await button.trigger('click')
+  expect(wrapper.vm.multiplication(1,2)).toBe(2)
+})
+
+test('trigger click event on button ', async () => {
+  const wrapper = shallowMount(FunctionPage)
+  const button = wrapper.find('button')
+  await button.trigger('click')
+  expect(wrapper.vm.divide(4,2)).toBe(2)
+})
